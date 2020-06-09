d1b952b667646db5744008f0519f983d4dfb417d
Juice Box <mikej10@vt.edu>
2020-06-09 16:27:57 -0400

    completed function for caching inverted matrix then solving the inverted matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## create object mi, make "set" a function of y where y is superassigned to x
+## create "get" as a function of x that creates the inverse of the matrix 
 makeCacheMatrix <- function(x = matrix()) {
-
+        mi <- NULL
+        set <- function(y) {
+                x <<- y
+                mi <<- NULL
+        }
+        get <- function() x
+        setmatrixinverse <- function(inverse) mi <<- matrix
+        getmatrixinverse <- function() mi
+        list(set = set, get = get,
+             setmatrixinverse = setmatrixinverse,
+             getmatrixinverse = getmatrixinverse)
 }
-
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        mi <- x$getmatrixinverse()
+        if(!is.null(mi)) {
+                message("getting cached data")
+                return(mi)
+        }
+        data <- x$get()
+        mi <- solve(data, ...)
+        x$setmatrixinverse(mi)
+        mi
 }
